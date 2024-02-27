6314fcd662625d40250978d052aa22e07e16045f
drmobile <drmobile@drmobiles-MacBook-Air.local>
2024-02-27 11:35:15 +0330

    initial project codebase

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Global.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
